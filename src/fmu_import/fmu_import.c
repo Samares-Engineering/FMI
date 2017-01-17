6510038815aed217380196bb0279230a6265f0b5
jmgauthier <jm.gauthier@samares-engineering.com>
2017-01-17 13:02:09 +0100

    Unzip fmu file ok

--- src/fmu_import/fmu_import.c
+++ src/fmu_import/fmu_import.c
@@ -5,7 +5,84 @@
 #include <stdarg.h>
 #include "fmu_import.h"
 
+// fmuFileName is an absolute path, e.g. "C:\test\a.fmu"
+// or relative to the current dir, e.g. "..\test\a.fmu"
+static char* getFmuPath(const char* fmuFileName){
+  /* Not sure why this is useful.  Just returning the filename. */
+  return strdup(fmuFileName);
+}
+
+static char* getTmpPath() {
+  char template[13];  // Lenght of "fmuTmpXXXXXX" + null
+  sprintf(template, "%s", "fmuTmpXXXXXX");
+  char *tmp = mkdtemp(template);
+  if (tmp==NULL) {
+    fprintf(stderr, "Couldn't create temporary directory\n");
+    exit(1);
+  }
+  char * results = calloc(sizeof(char), strlen(tmp) + 2);
+  strncat(results, tmp, strlen(tmp));
+  return strcat(results, "/");
+}
+
+int unzip(const char *zipPath, const char *outPath) {
+    int code;
+    char cwd[BUFSIZE];
+    int n;
+    char* cmd;
+
+    // remember current directory
+    if (!getcwd(cwd, BUFSIZE)) {
+      printf ("error: Could not get current directory\n");
+      return 0; // error
+    }
+        
+    // run the unzip command
+    n = strlen(UNZIP_CMD) + strlen(outPath) + 1 +  strlen(zipPath) + 16;
+    cmd = (char*)calloc(sizeof(char), n);
+    sprintf(cmd, "%s%s \"%s\" > /dev/null", UNZIP_CMD, outPath, zipPath); 
+    printf("cmd='%s'\n", cmd);
+    code = system(cmd);
+    free(cmd);
+    if (code!=SEVEN_ZIP_NO_ERROR) {
+        printf("%s: ", UNZIP_CMD);
+        switch (code) {
+            case 1:            printf("warning\n"); break;
+            case 2:            printf("error\n"); break;
+	    case 3:            printf("severe error\n"); break;
+            case 4:      
+            case 5:
+	    case 6:
+	    case 7:
+	      printf("out of memory\n"); break;
+   	    case 10:           printf("command line error\n"); break;
+	    default:           printf("unknown problem %d\n", code);
+        }
+    }
+    
+    // restore current directory
+    chdir(cwd);
+    
+    return (code==SEVEN_ZIP_NO_ERROR || code==SEVEN_ZIP_WARNING) ? 1 : 0;  
+}
+
 void loadFMU(const char* fmuFileName) {
-    printf("Loading FMU %s \n", fmuFileName);
-    printf("FMU %s Loaded\n", fmuFileName);
+    char* fmuPath;
+    char* tmpPath;
+    char* xmlPath;
+    char* dllPath;
+    const char *modelId;
+    
+    printf("Loading FMU %s. \n", fmuFileName);
+
+    // get absolute path to FMU, NULL if not found
+    fmuPath = getFmuPath(fmuFileName);
+    if (!fmuPath) exit(EXIT_FAILURE);
+    
+    tmpPath = getTmpPath();
+
+    printf("Unzip the %s file.\n", fmuFileName);
+    if (!unzip(fmuPath, tmpPath)) exit(EXIT_FAILURE);
+
+    printf("FMU %s Loaded.\n", fmuFileName);
 }
--- src/fmu_import/fmu_import.h
+++ src/fmu_import/fmu_import.h
@@ -1,1 +1,31 @@
+#define UNZIP_CMD "unzip -o -d "
+#define XML_FILE  "modelDescription.xml"
+#define RESULT_FILE "result.csv"
+#define BUFSIZE 4096
+
+// Linux
+#ifdef __x86_64
+#define DLL_DIR   "binaries/linux64/"
+#define DLL_DIR2   "binaries/linux32/"
+#else
+// It may be necessary to compile with -m32, see ../Makefile
+#define DLL_DIR   "binaries/linux32/"
+#define DLL_DIR2   "binaries/linux64/"
+#endif /*__x86_64*/
+	
+#define DLL_SUFFIX ".so"
+#define DLL_SUFFIX2 ".so"
+
+
+#define RESOURCES_DIR "resources\\"
+
+// return codes of the 7z command line tool
+#define SEVEN_ZIP_NO_ERROR 0 // success
+#define SEVEN_ZIP_WARNING 1  // e.g., one or more files were locked during zip
+#define SEVEN_ZIP_ERROR 2
+#define SEVEN_ZIP_COMMAND_LINE_ERROR 7
+#define SEVEN_ZIP_OUT_OF_MEMORY 8
+#define SEVEN_ZIP_STOPPED_BY_USER 255
+
+int unzip(const char *zipPath, const char *outPath);
 void loadFMU(const char *fmuFileName);
