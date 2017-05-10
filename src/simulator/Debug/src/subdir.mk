a7ca7edd4d317bce5996d3eb54750f45f94a316c
jmgauthier <jm.gauthier@samares-engineering.com>
2017-05-10 11:28:45 +0200

    debug folder

---
+++ src/simulator/Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/fmi2_api_impl.c \
+../src/test_fmi2_um_threads.c 
+
+OBJS += \
+./src/fmi2_api_impl.o \
+./src/test_fmi2_um_threads.o 
+
+C_DEPS += \
+./src/fmi2_api_impl.d \
+./src/test_fmi2_um_threads.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I/home/jmgauthier/Work/git_samares/FMI/fmusdk2/fmu20/src/shared -I/home/jmgauthier/Work/git_samares/FMI/fmusdk2/fmu20/src/shared/include -I/home/jmgauthier/Work/git_samares/FMI/fmusdk2/fmu20/src/shared/parser -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
