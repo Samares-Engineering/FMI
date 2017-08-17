8c698d17e41fd3e476f78e218b20910f950ecef5
jmgauthier <jm.gauthier@samares-engineering.com>
2017-08-17 14:46:43 +0200

    delete foo folder

--- src/foo/sources/RoomHeating_OM_RH_16dae.c
+++
@@ -1,9 +0,0 @@
-/* DAE residuals is empty */
- #include "RoomHeating_OM_RH_model.h"
-#ifdef __cplusplus
-extern "C" {
-#endif
-int RoomHeating_OM_RH_initializeDAEmodeData(DATA *inData, DAEMODE_DATA* daeModeData){ return -1; }
-#ifdef __cplusplus
-}
-#endif
